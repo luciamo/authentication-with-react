c9b5657512e597fbdbef9e4bfcecc8c20d367a14
Luciabo <lucianoeduardo@live.com>
2018-06-25 17:09:41 -0300

    Tá feio, mas tá funcionando

--- authentication-with-react/src/App.js
+++ authentication-with-react/src/App.js
@@ -1,45 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthService from './components/AuthService';
+import withAuth from './components/withAuth';
+const Auth = new AuthService();
 
 class App extends Component {
+
+  handleLogout(){
+    Auth.logout()
+    this.props.history.replace('/login');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Olá, este será um pequeno
-            exercício de autenticação.
-          </h1>
-        </header>
-        <div className="center">
-          <div className="card">
-              <h1>Login</h1>
-              <form>
-                  <input
-                      className="form-item"
-                      placeholder="Username goes here..."
-                      name="username"
-                      type="text"
-                      onChange={this.handleChange}
-                  />
-                  <input
-                      className="form-item"
-                      placeholder="Password goes here..."
-                      name="password"
-                      type="password"
-                      onChange={this.handleChange}
-                  />
-                  <input
-                      className="form-submit"
-                      value="SUBMIT"
-                      type="submit"
-                  />
-              </form>
-          </div>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome {this.props.user.username}</h2>
         </div>
+        <p className="App-intro">
+          <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default withAuth(App);
--- authentication-with-react/src/index.js
+++ authentication-with-react/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Login from './components/Login';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
-    <div>
-      <Route path='/' component={App} />
-    </div>
-  </Router>,
-  document.getElementById('root'));
+      <div>
+          <Route exact path="/" component={App} />
+          <Route exact path="/login" component={Login} />
+      </div>
+  </Router>
+  , document.getElementById('root')
+);
 registerServiceWorker();
